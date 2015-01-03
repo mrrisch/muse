ce4c4c34065ad41541fedd8663b7a7d3ce04ba1b
mrrisch <stephanrisch@gmx.de>
2015-01-03 14:57:33 +0100

    muse app first draft

---
+++ app/models/post.rb
@@ -0,0 +1,9 @@
+class Post < ActiveRecord::Base
+	belongs_to :user
+	has_many :comments
+	acts_as_votable
+
+	has_attached_file :image , :styles => { :medium => "700x500#", :small => "350x250>" }
+  	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
+end
